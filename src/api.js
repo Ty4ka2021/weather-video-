00bfbb09cfdfbbcde197b6658fbf264aa39aae74
Dmitriy <dm.hmara@gmail.com>
2023-06-20 14:45:48 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5d95365d57mshfec75e7e062828fp10961cjsne04db372b400',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "a08de665480b63003b05e7ee1153cca8"
+
---
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+import s from "./CurrentWeather.module.css"
+
+const CurrentWeather = ({ data }) => {
+	return (
+		<div className={s.CurrentWeather}>
+			<div className={s.top}>
+				<div>
+					<p className={s.city}>{data.city}</p>
+					<p className={s.desc}>{data.weather[0].description}</p>
+				</div>
+				<img className={s.weatherIcon} src={`icons/${data.weather[0].icon}.png`} alt="weather" />
+			</div>
+
+			<div className={s.bottom}>
+				<p className={s.temperature}>{Math.round(data.main.temp)}°C</p>
+				<ul className={s.details}>
+
+					<li className={s.parameterRow}>
+						<span className={s.parameterLabel}>Details</span>
+					</li>
+
+					<li className={s.parameterRow}>
+						<span className={s.parameterLabel}>Feels like</span>
+						<span className={s.parameterValue}>{Math.round(data.main.feels_like)}°C</span>
+					</li>
+
+					<li className={s.parameterRow}>
+						<span className={s.parameterLabel}>Wind</span>
+						<span className={s.parameterValue}>{data.wind.speed} m/s</span>
+					</li>
+
+					<li className={s.parameterRow}>
+						<span className={s.parameterLabel}>Humidity</span>
+						<span className={s.parameterValue}>{data.main.humidity} %</span>
+					</li>
+
+					<li className={s.parameterRow}>
+						<span className={s.parameterLabel}>Pressure</span>
+						<span className={s.parameterValue}>{data.main.pressure} hPa</span>
+					</li>
+
+
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default CurrentWeather
